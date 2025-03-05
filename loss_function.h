7f22b6273df333a5618afb068a408f46cb391bf1
Andriichuk Mikhail <moandriichuk@edu.hse.ru>
2025-03-05 17:56:36 +0300

    add loss_function impl

--- loss_function.h
+++ loss_function.h
@@ -1,24 +1,13 @@
-#include "external/eigen-3.4.0/Eigen/Dense"
-#include <functional>
-#include <cmath>
+#pragma once
 
-struct LossFunction {
-    std::function<double(const Eigen::MatrixXd&, const Eigen::MatrixXd&)> forward;
-    std::function<Eigen::MatrixXd(const Eigen::MatrixXd&, const Eigen::MatrixXd&)> backward;
+#include "types.h"
+
+namespace NeuralNet {
+
+class LossFunction {
+public:
+  double Difference(const Matrix &predictions, const Matrix &targets) const;
+  Matrix Gradient(const Matrix &predictions, const Matrix &targets) const;
 };
 
-inline LossFunction createMSE() {
-    LossFunction mse;
-
-    mse.forward = [](const Eigen::MatrixXd &predictions, const Eigen::MatrixXd &targets) -> double {
-        Eigen::MatrixXd diff = predictions - targets;
-        return diff.array().square().mean();
-    };
-
-    mse.backward = [](const Eigen::MatrixXd &predictions, const Eigen::MatrixXd &targets) -> Eigen::MatrixXd {
-        double n = static_cast<double>(predictions.rows());
-        return (2.0 / n) * (predictions - targets);
-    };
-
-    return mse;
-}
+} // namespace NeuralNet
---
+++ types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "external/eigen-3.4.0/Eigen/Dense"
+
+namespace NeuralNet {
+
+using Index = Eigen::Index;
+using Matrix = Eigen::MatrixXd;
+using Vector = Eigen::VectorXd;
+
+} // namespace NeuralNet
